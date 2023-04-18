d99907d3688332aaea9f0a8a2f3ee4d0ce1fffc0
guyl <guy1992l@gmail.com>
2023-04-18 17:22:09 +0300

    updated code and docs

--- src/project_setup.py
+++ src/project_setup.py
@@ -1,18 +1,54 @@
+import importlib
+
 import mlrun
 
 
+def assert_build():
+    for module_name in [
+        "transformers",
+        "datasets",
+        "optimum",
+        "onnx",
+        "onnxruntime",
+        "onnxoptimizer",
+    ]:
+        module = importlib.import_module(module_name)
+        print(module.__version__)
+
+
 def create_and_set_project(
-    name: str = "huggingface",
-    git_source: str = "git://github.com/davesh0812/mlrun-huggingface-demo.git#main",
-    default_image: str = "davesh0812/mlrun:huggingface-mlrun-demo",
-    user_project=False,
-    set_serving=True,
+    git_source: str,
+    name: str = "huggingface-mlrun-demo",
+    default_image: str = None,
+    user_project: bool = True,
+    set_serving: bool = True,
 ):
     # get/create a project and register the data prep and trainer function in it
     project = mlrun.get_or_create_project(
         name=name, context="./", user_project=user_project
     )
-    project.set_default_image(default_image)
+
+    if project.default_image is None:
+        if default_image is None:
+            print("Building image for the demo:")
+            image_builder = project.set_function(
+                "src/project_setup.py",
+                name="image-builder",
+                handler="assert_build",
+                kind="job",
+                image="mlrun/ml-models",
+                requirements=[
+                    "transformers~=4.26.0",
+                    "datasets~=2.10.1",
+                    "optimum~=1.6.4",
+                    "onnx~=1.10.1",
+                    "onnxruntime~=1.11.1",
+                    "onnxoptimizer~=0.3.8",
+                ],
+            )
+            assert image_builder.deploy()
+            default_image = image_builder.spec.image
+        project.set_default_image(default_image)
 
     project.set_source(git_source, pull_at_runtime=True)
 
